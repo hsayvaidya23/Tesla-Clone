e5b50b4c86331adb1bad6e340b8241513fcde2a3
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-07-08 00:08:18 +0530

    Menu component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState} from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <div className="app">
         
         <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        {/* headerBlock */}
+        {isMenuOpen && <Menu />}
       </div>
     </Router>
   );
