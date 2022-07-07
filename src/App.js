133c0a286d585700aac2f446be966dbb8b6d1c2f
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-07-07 23:15:50 +0530

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="App">
-    <h1>Tesla Clone 🚀</h1>
-    </div>
+    <Router>
+      <div className="app">
+        
+        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        {/* headerBlock */}
+      </div>
+    </Router>
   );
 }
 
