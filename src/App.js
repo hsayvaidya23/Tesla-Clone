d9054dd9e93258093e005e46a7977487b2011fc4
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-07-09 22:34:39 +0530

    Login component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import HeaderBlock from './components/HeaderBlock/HeaderBlock';
+import Login from './components/Login/Login';
 
 
 function App() {
@@ -12,10 +13,17 @@ function App() {
   return (
     <Router>
       <div className="app">
-        
-        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        {isMenuOpen && <Menu />}
-        <HeaderBlock />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+              {isMenuOpen && <Menu />}
+              <HeaderBlock />
+            </>
+          } />
+          <Route exact path='/login' element={<Login/>} />
+        </Routes>
+
       </div>
     </Router>
   );
